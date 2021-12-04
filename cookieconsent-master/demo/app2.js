c156d1af2e4e6b3135feb30f112e15ec6d16fe7a
XavierDatacom <xavier.laffile@gmail.com>
2021-12-04 13:03:22 +0100

    Update app2.js

--- cookieconsent-master/demo/app2.js
+++ cookieconsent-master/demo/app2.js
@@ -1,5 +1,10 @@
 // obtain cookieconsent plugin
 var cc = initCookieConsent();
+var lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ';
+var short_lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis exercitation. ';
+var super_short_lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+var loaded_clarity = false;
+// run plugin with config object
 
 // run plugin with config object
 cc.run({
